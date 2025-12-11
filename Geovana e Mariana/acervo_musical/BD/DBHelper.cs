d03b8859ed133e3c7dedec0f01ea0277269c5098
Geovana Lessa <lessageovanac@gmail.com>
2025-12-11 08:55:28 -0300

    Add files via upload

---
+++ Geovana e Mariana/acervo_musical/BD/DBHelper.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace AcervoMusical.BD
+{
+    public static class DBHelper
+    {
+        private static string connStr = "server=localhost;database=acervo;uid=root;pwd='';";
+
+        public static MySqlConnection GetConnection()
+        {
+            var conn = new MySqlConnection(connStr);
+            return conn;
+        }
+    }
+}
